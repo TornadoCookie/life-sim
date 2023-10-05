bd4c13b15d58eff53d2c873d566d6ad864842ba0
Helix Graziani <helix.graziani@hotmail.com>
2023-10-05 19:35:25 -0400

    life: Use format strings.

--- backend/life/life.cpp
+++ backend/life/life.cpp
@@ -1,5 +1,6 @@
 #include "life.hpp"
 #include <iostream>
+#include <memory>
 
 PlayerLife::PlayerLife(UrgentLifeEventLogger *u_logger, YearLogger *y_logger)
 {
@@ -7,6 +8,17 @@ PlayerLife::PlayerLife(UrgentLifeEventLogger *u_logger, YearLogger *y_logger)
     name_generator = new NameGenerator;
     urgent_life_event_logger = u_logger;
     year_logger = y_logger;
+}
+
+template<typename ... Args>
+std::string string_format( const std::string& format, Args ... args )
+{
+    int size_s = std::snprintf( nullptr, 0, format.c_str(), args ... ) + 1; // Extra space for '\0'
+    if( size_s <= 0 ){ throw std::runtime_error( "Error during formatting." ); }
+    auto size = static_cast<size_t>( size_s );
+    std::unique_ptr<char[]> buf( new char[ size ] );
+    std::snprintf( buf.get(), size, format.c_str(), args ... );
+    return std::string( buf.get(), buf.get() + size - 1 ); // We don't want the '\0' inside
 }
 
 void PlayerLife::StartRandomLife()
@@ -46,53 +58,22 @@ void PlayerLife::StartRandomLife()
     birth_event->default_option = 1;
     birth_event->options = {"Okay"};
     birth_event->title = "A New Life";
-    birth_event->content = "You were born a ";
-    birth_event->content.append(gender == Gender::Male ? "male" : "female");
-    birth_event->content.append(", in ");
-    birth_event->content.append(nation.shouldHaveTheBefore  ? "the " : " ");
-    birth_event->content.append(nation.name);
-    birth_event->content.append(".\n\nYou have been named ");
-    birth_event->content.append(first_name);
-    birth_event->content.append(" ");
-    birth_event->content.append(last_name);
-    birth_event->content.append(".\n\nYou are ");
-    birth_event->content.append(nation.demonym);
-    birth_event->content.append(".");
 
+    std::string gender_str = gender == Gender::Male ? "male" : "female";
+    std::string nation_the_str = nation.shouldHaveTheBefore  ? "the " : " ";
+    birth_event->content = string_format("You were born a %s, in %s%s.\n\nYou have been named %s %s.\n\nYou are %s.",
+        gender_str.c_str(), nation_the_str.c_str(), nation.name.c_str(),
+        first_name.c_str(), last_name.c_str(), nation.demonym.c_str());
     urgent_life_event_logger->PromptUrgentLifeEvent(birth_event);
 
-    year_logger->AddToThisYearLog("You were born a ");
-
-    if (gender == Gender::Male)
-        year_logger->AddToThisYearLog("male");
-    else if (gender == Gender::Female)
-        year_logger->AddToThisYearLog("female");
-
-    year_logger->AddToThisYearLog(", in ");
-    if (nation.shouldHaveTheBefore)
-        year_logger->AddToThisYearLog("the ");
-    year_logger->AddToThisYearLog(nation.name);
-    year_logger->AddToThisYearLog(".\n\nYour name is ");
-    year_logger->AddToThisYearLog(first_name);
-    year_logger->AddToThisYearLog(" ");
-    year_logger->AddToThisYearLog(last_name);
-    year_logger->AddToThisYearLog(".\n\n");
-
-    year_logger->AddToThisYearLog("You are the son of:\n");
-
-    year_logger->AddToThisYearLog(father->first_name);
-    year_logger->AddToThisYearLog(" ");
-    year_logger->AddToThisYearLog(father->last_name);
-    year_logger->AddToThisYearLog(", ");
-    year_logger->AddToThisYearLog(std::to_string(father->age));
-    year_logger->AddToThisYearLog(".\n");
-
-    year_logger->AddToThisYearLog(mother->first_name);
-    year_logger->AddToThisYearLog(" ");
-    year_logger->AddToThisYearLog(mother->last_name);
-    year_logger->AddToThisYearLog(", ");
-    year_logger->AddToThisYearLog(std::to_string(mother->age));
-    year_logger->AddToThisYearLog(".\n\n");
+    year_logger->AddToThisYearLog(
+        string_format("You were born a %s in %s%s.\n\n Your name is %s %s.\n\n",
+        gender_str.c_str(), nation_the_str.c_str(), nation.name.c_str(), first_name.c_str(),
+        last_name.c_str()));
+    year_logger->AddToThisYearLog(
+        string_format("You are the son of:\n%s %s, %d.\n%s %s, %d.\n\n",
+        father->first_name.c_str(), father->last_name.c_str(), father->age,
+        mother->first_name.c_str(), mother->last_name.c_str(), mother->age));
 }
 
 void age_up_life(Life *life)
