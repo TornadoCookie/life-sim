a23f4d493ed17200067f36d1c43ad58f9772355a
Helix Graziani <helix.graziani@hotmail.com>
2023-10-24 17:45:56 -0400

    events: Urgent life events are no longer pointers.

--- backend/events/urgent_life_event.cpp
+++ backend/events/urgent_life_event.cpp
@@ -1,17 +1,18 @@
 #include "events.hpp"
 #include <iostream>
 
-int UrgentLifeEventLogger::PromptUrgentLifeEvent(UrgentLifeEvent *event)
+int UrgentLifeEventLogger::PromptUrgentLifeEvent(UrgentLifeEvent event)
 {
     if (!urgent_life_event_callback)
     {
-        std::cout << "Urgent life event " << event->title << " could not be prompted because the frontend has not set an urgent life event callback." << std::endl;
+        std::cout << "Urgent life event \"" << event.title << "\" could not be prompted because the frontend has not set an urgent life event callback." << std::endl;
+        return -1;
     }
 
     return urgent_life_event_callback(event);
 }
 
-void UrgentLifeEventLogger::RegisterUrgentLifeEventCallback(int(*callback)(UrgentLifeEvent*))
+void UrgentLifeEventLogger::RegisterUrgentLifeEventCallback(int(*callback)(UrgentLifeEvent))
 {
     urgent_life_event_callback = callback;
 }
